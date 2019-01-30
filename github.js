2d40a49e2d11bb8b79a69398575500e272d1cef7
Evan Tanner <etanner@xpressdocs.com>
2019-01-30 11:27:28 -0600

    added commments to github.js

--- github.js
+++ github.js
@@ -1,12 +1,13 @@
 class Github {
-
+ 
     constructor() {
         this.client_id = '8998b0d708acbbdbbaaa';
         this.client_secret = '764b5616a0a4eb2665ab4e960708b83ea191426a';
         this.repos_count = 5;
         this.repos_sort = 'created: asc';
     }
-
+    
+    //getuser
     async getUser(user) {
 
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
